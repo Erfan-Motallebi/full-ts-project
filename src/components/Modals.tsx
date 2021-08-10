09495c129fda9214ae1f36928ac8ee1725f6ab7c
Erfan Motallebi <Erfan.Motallebi1990@gmail.co>
2021-08-10 19:24:36 +0430

    added the add-remove modal concering material ui

---
+++ src/components/Modals.tsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import Container from "@material-ui/core/Container";
+import Snackbar from "@material-ui/core/Snackbar";
+import MuiAlert, { AlertProps } from "@material-ui/lab/Alert";
+import { makeStyles, Theme } from "@material-ui/core/styles";
+import { useEffect } from "react";
+
+function Alert(props: AlertProps) {
+  return <MuiAlert elevation={3} variant="filled" {...props} />;
+}
+
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    width: "100%",
+    "& > * + *": {
+      marginTop: theme.spacing(2),
+    },
+  },
+}));
+
+type Props = {
+  modalContent: string;
+};
+
+const Modals: React.FC<Props> = ({ modalContent }) => {
+  const classes = useStyles();
+  const [modalOpen, setModal] = useState<boolean>(false);
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const handleOpen = () => {
+    setModal(true);
+  };
+
+  const handleClose = (event?: React.SyntheticEvent, reason?: string) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setModal(false);
+  };
+
+  useEffect(() => {
+    const modalTimeOut = setTimeout(() => {
+      handleOpen();
+    }, 500);
+    return () => {
+      clearTimeout(modalTimeOut);
+    };
+  }, []);
+
+  return (
+    <Container className={classes.root}>
+      <Snackbar open={modalOpen} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="success">
+          {modalContent}
+        </Alert>
+      </Snackbar>
+    </Container>
+  );
+};
+
+export default Modals;
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -16,6 +16,18 @@ import AssignmentIndIcon from "@material-ui/icons/AssignmentInd";
 import ListItemText from "@material-ui/core/ListItemText";
 import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
 import DeleteIcon from "@material-ui/icons/Delete";
+import Modals from "./Modals";
+import Dialog from "@material-ui/core/Dialog";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogActions from "@material-ui/core/DialogActions";
+import { TransitionProps } from "@material-ui/core/transitions/transition";
+import Slide from "@material-ui/core/Slide";
+import Badge from "@material-ui/core/Badge";
+import WorkIcon from "@material-ui/icons/Work";
+import InputAdornment from "@material-ui/core/InputAdornment";
+
 const Wrapper = styled.div`
   max-width: 1400px;
   margin: 0 auto;
@@ -24,6 +36,7 @@ const Wrapper = styled.div`
 const StyledTextField = styled(TextField)`
   width: 85%;
   background: #fff;
+  padding: 25px;
 `;
 
 const StyledForm = styled.form`
@@ -37,48 +50,95 @@ const StyledButton = styled(Button)`
   border-radius: 0 2px 2px 0;
 `;
 
+const StyledBadge = styled(Badge)`
+  font-size: 3rem;
+  margin-right: 20px;
+`;
+
+const Transition = React.forwardRef(function Transition(
+  props: TransitionProps & { children?: React.ReactElement<any, any> },
+  ref: React.Ref<unknown>
+) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
 function Todos(): JSX.Element {
   const [value, setValue] = useState<TodoStateType>({
     inputValue: "",
     condition: false,
+    modal: {
+      content: "",
+      state: false,
+    },
+    open: false,
   });
+
+  const agree = () => {
+    setValue({
+      open: false,
+      modal: {
+        content: "Added",
+        state: true,
+      },
+      inputValue: "",
+      condition: true,
+    });
+    addTodo(value.inputValue);
+  };
+
+  const closeModal = () => {
+    setValue({
+      ...value,
+      open: false,
+    });
+  };
+
   const { todoList } = useSelector((state: RState) => state.todo);
   const dispatch = useDispatch();
   const { addTodo, removeTodo } = bindActionCreators(actionCreators, dispatch);
 
-  // const todoListContainer = () => {
-  //   const lists = localStorage.getItem('lists')
-  //   if(lists) {
-  //     addTodo(lists)
-  //   }
-  //   else {
-  //     addTodo()
-  //   }
-  // }
-
   const onSubmitHandler = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    addTodo(value.inputValue);
     setValue({
-      inputValue: "",
-      condition: true,
+      ...value,
+      open: true,
     });
   };
 
-  useEffect(() => {
-    localStorage.setItem("lists", JSON.stringify(todoList));
-  });
+  // useEffect(() => {
+  //   localStorage.setItem("lists", JSON.stringify(todoList));
+  // });
   return (
     <React.Fragment>
       <Wrapper>
-        <StyledForm autoComplete="off" onSubmit={onSubmitHandler}>
+        <StyledForm
+          autoComplete="off"
+          onSubmit={(e: React.MouseEvent<HTMLFormElement>) =>
+            onSubmitHandler(e)
+          }
+        >
           <StyledTextField
             id="outlined-helperText"
             label="Task"
             variant="outlined"
             placeholder="Your Task "
             value={value.inputValue}
-            onChange={(e) => setValue({ ...value, inputValue: e.target.value })}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <StyledBadge badgeContent={todoList.length} color="secondary">
+                    <WorkIcon />
+                  </StyledBadge>
+                </InputAdornment>
+              ),
+            }}
+            onChange={(e) =>
+              setValue({
+                ...value,
+                inputValue: e.target.value,
+                modal: { content: "", state: false },
+              })
+            }
           />
           <StyledButton
             variant="contained"
@@ -93,7 +153,7 @@ function Todos(): JSX.Element {
           {value.condition && (
             <List
               style={{
-                backgroundColor: "#FEFFE2",
+                backgroundColor: "#DAD5AB",
                 borderRadius: 5,
                 padding: 10,
                 margin: 5,
@@ -103,20 +163,34 @@ function Todos(): JSX.Element {
                 return (
                   <ListItem key={list.id} style={{ margin: 5 }}>
                     <ListItemAvatar>
-                      <Avatar style={{ backgroundColor: "#DAD5AB" }}>
+                      <Avatar style={{ backgroundColor: "#b4a730" }}>
                         <AssignmentIndIcon
                           style={{ backgroundColor: "#082032" }}
                         />
                       </Avatar>
                     </ListItemAvatar>
-                    <ListItemText
-                      style={{ fontSize: "1.2rem" }}
-                      primary={list.body}
-                    />
+                    <ListItemText primary={list.body} />
                     <ListItemSecondaryAction>
                       <Button
                         onClick={() => {
                           removeTodo(list.id as string);
+                          setValue({
+                            ...value,
+                            condition: false,
+                            modal: {
+                              content: "Removed",
+                              state: true,
+                            },
+                          });
+                        }}
+                        onMouseOver={() => {
+                          setValue({
+                            ...value,
+                            modal: {
+                              content: "",
+                              state: false,
+                            },
+                          });
                         }}
                       >
                         <DeleteIcon />
@@ -129,6 +203,33 @@ function Todos(): JSX.Element {
           )}
         </Container>
       </Wrapper>
+      <Dialog
+        open={value.open}
+        TransitionComponent={Transition}
+        keepMounted
+        onClose={closeModal}
+        aria-labelledby="alert-dialog-slide-title"
+        aria-describedby="alert-dialog-slide-description"
+      >
+        <DialogTitle id="alert-dialog-slide-title">
+          {"Do you want to accept ?"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-slide-description">
+            {value.inputValue}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={closeModal} color="primary">
+            Disagree
+          </Button>
+          <Button onClick={agree} color="primary">
+            Agree
+          </Button>
+        </DialogActions>
+      </Dialog>
+
+      {value.modal.state && <Modals modalContent={value.modal.content} />}
     </React.Fragment>
   );
 }
