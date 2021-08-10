fc6b58994985cc2e01eeaa93be4e5e53e573dba0
Erfan Motallebi <Erfan.Motallebi1990@gmail.co>
2021-08-10 19:28:51 +0430

    removed unused hooks

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { bindActionCreators } from "redux";
 import { actionCreators } from "../redux";
